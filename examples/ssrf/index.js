50b318551eae6a3ae3ab43d4c74a5749e6f5d10a
Zhang Zeyu <zhangzeyu2001@gmail.com>
2022-10-23 02:14:01 +0800

    IP / host matching

--- examples/ssrf/index.js
+++ examples/ssrf/index.js
@@ -1,17 +1,50 @@
-const axios = require('axios');
-const { Axiom } = require('zeno');
+(async () => {
 
-new Axiom({
-    whitelist: [],
-    blacklist: [],
-})
+    const axios = require('axios');
+    const { Axiom } = require('zeno');
 
-axios.get("http://example.com")
-    .then((response) => {
-        console.log(response.data);
+    new Axiom({
+        acl: [
+            {
+                match: "evil.github.com",
+                action: "deny"
+            },
+            {
+                match: "*.github.com",
+                action: "allow"
+            },
+            {
+                match: "2001:db8::/32",
+                action: "deny"
+            },
+            {
+                match: "1.0.0.0/8",
+                action: "deny"
+            },
+            {
+                match: "*",
+                action: "allow"
+            }
+        ]
     })
-    .catch((error) => {
-        console.log(`${error.toString().split('\n')[0]}`);
-    })
 
+    const urls = [
+        "http://example.com/",
+        "http://google.com",
+        "http://evil.github.com",
+        "http://gtfobins.github.io",
+        "http://1.1.1.1",
+        "http://127.0.0.1",
+    ]
+
+    for (const url of urls) {
+        await axios.get(url)
+            .then((response) => {
+                console.log(`[+] ${url} => ${response.status}`)
+            })
+            .catch((error) => {
+                console.log(`${error.toString().split('\n')[0]}`);
+            })
+    }
+})();
 
--- modules/Axiom/ssrf.ts
+++ modules/Axiom/ssrf.ts
@@ -1,14 +1,15 @@
 import net from 'net';
 import http from 'http';
 import https from 'https';
+import ipaddr from 'ipaddr.js';
+import { isCIDR } from '../utils.js';
 
 interface axiomArgs {
-    blacklist: string[];
-    whitelist: string[];
+    acl: { match: string, action: string }[];
 }
 
 interface Axiom {
-    acl: { host: string, action: string }[];
+    acl: { match: string | [ipaddr.IPv4 | ipaddr.IPv6, number], action: string, type: string }[];
 }
 
 interface httpAgent extends http.Agent {
@@ -21,7 +22,31 @@ interface httpsAgent extends https.Agent {
 
 class Axiom implements Axiom {
     constructor(args: axiomArgs) {
-        this.acl = args.acl;
+        this.acl = [];
+
+        for (let i = 0; i < args.acl.length; i++) {
+
+            let match, action, type;
+
+            action = args.acl[i].action;
+
+            if (isCIDR(args.acl[i].match)) {
+                match = ipaddr.parseCIDR(args.acl[i].match);
+                type = match[0].kind();
+            } else if (ipaddr.isValid(args.acl[i].match)) {
+                match = ipaddr.process(args.acl[i].match);
+                type = match.kind();
+            } else {
+                match = args.acl[i].match;
+                type = 'domain';
+            }
+
+            this.acl.push({
+                match: match,
+                action: action,
+                type: type
+            })
+        }
 
         // @see https://github.com/facebook/flow/issues/7670
         // @ts-expect-error Node.js version compatibility
@@ -31,23 +56,48 @@ class Axiom implements Axiom {
         https.globalAgent = this.createCustomAgent(https.globalAgent);
     }
 
-    private checkIp = (ip: string) => {
+    private checkDomain = (domain: string, match: string) => {
+        if (match.startsWith('*')) {
+            match = match.slice(1);
+            return domain.endsWith(match);
+        }
+        return domain === match;
+    }
+
+    private checkACL = (ip: string, domain: string) => {
+        let ipAddr = ipaddr.process(ip);
+        for (let i = 0; i < this.acl.length; i++) {
+            if (this.acl[i].type === 'ipv4' && ipAddr.kind() === 'ipv4' && ipAddr.match(this.acl[i].match as [ipaddr.IPv4, number])) {
+                return this.acl[i].action === 'allow';
+            } else if (this.acl[i].type === 'ipv6' && ipAddr.kind() === 'ipv6' && ipAddr.match(this.acl[i].match as [ipaddr.IPv6, number])) {
+                return this.acl[i].action === 'allow';
+            } else if (this.acl[i].type === 'domain' && this.checkDomain(domain, this.acl[i].match as string)) {
+                return this.acl[i].action === 'allow';
+            }
+        }
+
+        // default deny
         return false;
     }
 
     public createCustomAgent = (agent: httpAgent | httpsAgent) => {
         const createConnection = agent.createConnection;
         agent.createConnection = (options, callback) => {
+            
+            // If an IP address is provided, no lookup is performed.
             const { host: address } = options;
-            if (!this.checkIp(address)) {
+            if (!this.checkACL(address, address)) {
                 throw new Error(`Call to ${address} is blocked.`);
             }
-            const socket = createConnection.call(this, options, callback);
-            socket.on('lookup', (error, address) => {
-                if (error || this.checkIp(address)) {
+            
+            const socket = createConnection.call(agent, options, callback);
+
+            // Check IP address at lookup time
+            socket.on('lookup', (error, address, family, host) => {
+                if (error || this.checkACL(address, host)) {
                     return false;
                 }
-                return socket.destroy(new Error(`Call to ${address} is blocked.`));
+                return socket.destroy(new Error(`Call to ${host} is blocked.`));
             });
             return socket;
         }
