15a5cf4571368e8b656320db99980f24433e243b
TheMythologist <leekaixuan2001@gmail.com>
2022-10-25 12:28:42 +0800

    refactor: improve test messages for Axiom

--- modules/Axiom/tests/axiom.test.ts
+++ modules/Axiom/tests/axiom.test.ts
@@ -1,16 +1,16 @@
 import { axiom } from '../../../';
 import { InvalidACLRule } from '../axiom';
 
-describe('#validateDomainAcl', () => {
+describe('validateDomainAcl', () => {
   const ax = axiom([]);
 
-  it('valid domains', () => {
+  it('should return true for valid domains', () => {
     expect(ax['validateDomainAcl']('*')).toBe(true);
     expect(ax['validateDomainAcl']('example.com')).toBe(true);
     expect(ax['validateDomainAcl']('*.example.com')).toBe(true);
   });
 
-  it('invalid domains', () => {
+  it('should return false for invalid domains', () => {
     expect(ax['validateDomainAcl']('**.example.com')).toBe(false);
     expect(ax['validateDomainAcl']('example.*.com')).toBe(false);
     expect(ax['validateDomainAcl']('*.api.*.com')).toBe(false);
@@ -20,13 +20,13 @@ describe('#validateDomainAcl', () => {
   });
 });
 
-describe('#InvalidACLRule', () => {
+describe('InvalidACLRule', () => {
   // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
   const badAxiomConstructor = () => {
     axiom([{ match: 'example.*', action: 'allow' }]);
   };
 
-  it('invalid ACL rules', () => {
+  it('should be thrown for invalid ACL rules', () => {
     expect(badAxiomConstructor).toThrow(new InvalidACLRule('example.*'));
   });
 });
