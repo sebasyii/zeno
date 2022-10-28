4e66bc3130b3926f7609908659b910049f234e3e
TheMythologist <leekaixuan2001@gmail.com>
2022-10-28 11:12:31 +0800

    tests: implement tests for default checkACL

--- modules/Axiom/tests/axiom.test.ts
+++ modules/Axiom/tests/axiom.test.ts
@@ -2,21 +2,16 @@ import { axiom } from '../../../';
 import { InvalidACLRule } from '../axiom';
 
 describe('validateDomainAcl', () => {
-  const ax = axiom([]);
+  const ax = axiom();
 
   it('should return true for valid domains', () => {
-    expect(ax['validateDomainAcl']('*')).toBe(true);
-    expect(ax['validateDomainAcl']('example.com')).toBe(true);
-    expect(ax['validateDomainAcl']('*.example.com')).toBe(true);
+    const validDomains = ['*', 'example.com', '*.example.com']
+    validDomains.forEach((validDomain) => expect(ax['validateDomainAcl'](validDomain)).toBe(true))
   });
 
   it('should return false for invalid domains', () => {
-    expect(ax['validateDomainAcl']('**.example.com')).toBe(false);
-    expect(ax['validateDomainAcl']('example.*.com')).toBe(false);
-    expect(ax['validateDomainAcl']('*.api.*.com')).toBe(false);
-    expect(ax['validateDomainAcl']('ex*ample.com')).toBe(false);
-    expect(ax['validateDomainAcl']('*example.com')).toBe(false);
-    expect(ax['validateDomainAcl']('example.*')).toBe(false);
+    const invalidDomains = ['**.example.com', 'example.*.com', '*.api.*.com', 'ex*ample.com', '*example.com', 'example.*']
+    invalidDomains.forEach((invalidDomain) => expect(ax['validateDomainAcl'](invalidDomain)).toBe(false))
   });
 });
 
@@ -67,7 +62,25 @@ describe('checkACL', () => {
     expect(ax['checkACL']('1.0.0.1')).toBe(false);
   })
 
-  it ('should return false for denied ipv6 networks', () => {
+  it('should return false for denied ipv6 networks', () => {
     expect(ax['checkACL']('2001:db8::1')).toBe(false);
+  })
+});
+
+describe('default checkACL', () => {
+  const ax = axiom();
+
+  it('should return false for ipv4 special ranges', () => {
+    const ipv4SpecialIPs = ['0.0.0.0', '127.0.0.2', '192.168.0.5', '169.254.0.0', '198.51.100.0', '255.255.255.255']
+    ipv4SpecialIPs.forEach((ipv4SpecialIP) => expect(ax['checkACL'](ipv4SpecialIP)).toBe(false))
+  })
+
+  it('should return false for ipv6 special ranges', () => {
+    const ipv6SpecialIPs = ['::1', '64:ff9b::', '100::', '2001:3::1', 'fc00::11:', 'fe80::',]
+    ipv6SpecialIPs.forEach((ipv6SpecialIP) => expect(ax['checkACL'](ipv6SpecialIP)).toBe(false))
+  })
+
+  it('should return true for non-special IPs', () => {
+    expect(ax['checkACL']('158.25.147.235', 'github.com')).toBe(true)
   })
 });
