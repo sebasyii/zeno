39724ad9e23fd296e7d6eebb3feee228269d38ca
Zhang Zeyu <zhangzeyu2001@gmail.com>
2022-10-31 18:39:11 +0800

    fix: example import path

--- examples/indirect/src/index.js
+++ examples/indirect/src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
 const util = require('util');
-const { Indirect } = require('../../../');
+const { Indirect } = require('@zenots/zeno');
 
 const app = express();
 
